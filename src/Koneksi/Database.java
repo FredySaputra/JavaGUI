66eb8928e2472a64b5020533f2a4cf996a617f19
Mahasiswa <Mahasiswa@L02PC23>
2026-03-09 13:18:39 +0700

    Initial Commit

---
+++ src/Koneksi/Database.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Database {
+    static Connection conn;
+    
+    public static Connection KoneksiDB(){
+        if(conn==null){
+            try{
+                Class.forName("com.mysql.jdbc.driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/db_akademik_2311500140","root","");
+                JOptionPane.showMessageDialog(null, "Koneksi Berhasil!","Pesan",JOptionPane.INFORMATION_MESSAGE);
+            }
+            catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Koneksi Tidak Berhasil","Pesan",JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("Error"+e.getMessage());
+            }
+        }
+        return conn;
+    }
+}
